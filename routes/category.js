5913348fcd2945791f56b9789340ad0fbf88fe84
rowelalvero <alvero.323400@sjdelmonte.sti.edu.ph>
2024-11-09 06:07:44 +0800

    Added coords to include in fetch

--- routes/category.js
+++ routes/category.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const categoryController = require('../controllers/categoryController');
 
-router.post("/categories", categoryController.createCategory);
-
+router.post("/", categoryController.createCategory);
 
 router.get("/", categoryController.getAllCategories);
 
