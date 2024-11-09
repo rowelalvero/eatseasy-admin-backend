ff994cddcf4fcadac666f78c672dc5d4ec3125d3
rowelalvero <alvero.323400@sjdelmonte.sti.edu.ph>
2024-11-09 23:59:50 +0800

    Added coords to include in fetch

--- models/Drivers.js
+++ models/Drivers.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const driverSchema = new mongoose.Schema({
     driver: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    vehicleType: { type: String, required: true, enum: ['Bike', 'Car', 'Scooter', 'Drone'] },
+    vehicleType: { type: String, required: true, enum: ['Bike', 'Car', 'Scooter'] },
     vehicleName: {type: String},
     licenseNumber: {type: String},
     licenseExpireDate: {type: String},
@@ -15,11 +15,12 @@ const driverSchema = new mongoose.Schema({
         longitude: { type: Number, required: true },
     },
     isAvailable: { type: Boolean, default: true },
-    verification: {type: String ,default: "Pending", enum: ["Pending", "Verified", "Rejected"]},
-    verificationMessage: {type: String, default: "Please allow up to 24 hours for your verification to be processed. You will receive a notification once your verification is complete."},
-    rating: { type: Number, min: 1, max: 5, default: 3},
+    verification: { type: String, default: "Pending", enum: ["Pending", "Verified", "Rejected"] },
+    verificationMessage: { type: String, default: "Please allow up to 24 hours for your verification to be processed. You will receive a notification once your verification is complete." },
+    rating: { type: Number, min: 1, max: 5, default: 3 },
     totalDeliveries: { type: Number, default: 0 },
-    profileImage: {type:String, default: "https://d326fntlu7tb1e.cloudfront.net/uploads/cf503f8d-3318-4306-9e2c-a53efb7d585b-avatar.png"},
+    totalEarnings: { type: Number, default: 0.0 },
+    profileImage: { type: String, default: "https://d326fntlu7tb1e.cloudfront.net/uploads/cf503f8d-3318-4306-9e2c-a53efb7d585b-avatar.png" },
     isActive: { type: Boolean, default: false } // To track if the driver is currently active on the platform
 });
 
