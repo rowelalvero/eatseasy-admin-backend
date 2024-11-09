204ec8537c43deda7f4f1d3cbb98d79b91611178
rowelalvero <alvero.323400@sjdelmonte.sti.edu.ph>
2024-11-10 00:58:39 +0800

    Added coords to include in fetch

--- models/Order.js
+++ models/Order.js
@@ -1,38 +1,44 @@
 const mongoose = require('mongoose');
 
-
 const orderItemSchema = new mongoose.Schema({
-    foodId: {type: mongoose.Schema.Types.ObjectId, ref: 'Food'},
-    quantity: {type: Number, default: 1},
-    price: {type: Number, required: true},
-    additives: {type: Array},
-    instructions: {type: String, default: ''}
+    foodId: { type: mongoose.Schema.Types.ObjectId, ref: "Food" },
+    quantity: { type: Number, required: true },
+    price: { type: Number, required: true },
+    unitPrice:{type:Number, default: 0},
+    additives: { type: Array },
+    instructions: {type: String, default: ''},
+    cartItemId: { type: mongoose.Schema.Types.ObjectId, ref: "Cart" },
+    customAdditives: { type: Map, of: mongoose.Schema.Types.Mixed }, // Update here
 });
 
-const OrderSchema = new mongoose.Schema({
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+const orderSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     orderItems: [orderItemSchema],
-    orderTotal: {type: Number, required: true},
-    deliveryFee: {type: Number, required: true},
-    grandTotal: {type: Number, required: true},
+    orderTotal: { type: Number, required: true },
+    deliveryFee: { type: Number, required: true },
+    grandTotal: { type: Number, required: true },
+    deliveryOption: { type: String },
+    deliveryDate: { type: String },
     deliveryAddress: {
-        type: mongoose.Schema.Types.ObjectId, ref: 'Address', required: true 
-    }, 
-    restaurantAddress: {
-        type: String, required: true,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Address",
+        required: true
     },
-    paymentMethod: {type: String, required: true},
-    paymentStatus: {type: String, default: 'Pending', enum: ['Pending', 'Completed', 'Failed']},
-    orderStatus: {type: String, default: 'Pending', enum: ['Placed', 'Preparing', 'Manual',"Ready", "Out_for_Delivery", 'Delivered', 'Cancelled',]},
-    restaurantId: {type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant'},
+    restaurantAddress: {type: String, required: true},
+    paymentMethod: { type: String },
+    paymentStatus: { type: String, default: "Pending", enum: ["Pending", "Completed", "Failed"] },
+    orderStatus: { type: String, default: "Placed", enum: ["Placed", "Preparing", "Manual" ,"Out_for_Delivery", "Ready", "Cancelled", "Delivered"] },
+    driverStatus: { type: String, default: "Vacant", enum: ["Vacant", "Picking", "Delivering", "Delivered"] },
+    orderDate: { type: Date, default: Date.now },
+    restaurantId: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurant"},
     restaurantCoords: [Number],
     recipientCoords: [Number],
-    driverId: {type: String , default: ''},
-    rating: {type: Number, min: 1, max: 5, default: 3},
-    feedback: {type: String, },
-    promoCode: {type: String},
-    discountAmount: {type: Number},
-    notes: {type: String}
-}, { timestamps: true });
+    driverId: { type: mongoose.Schema.Types.ObjectId, ref: "Driver" },
+    rating: { type: Number, min: 1, max: 5 },
+    feedback: String,
+    promoCode: String,
+    discountAmount: Number,
+    notes: String
+}, {timestamps: true});
 
-module.exports = mongoose.model('Order', OrderSchema);
+module.exports = mongoose.model('Order', orderSchema);
